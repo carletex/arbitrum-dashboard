e68961fdc7df5b7b6b06e1852518d0a1ced66e3f
Shiv Bhonde <shivbhonde04@gmail.com>
2026-01-23 10:14:06 +0530

    use new types for request and response

--- packages/nextjs/app/api/auth/[...nextauth]/route.ts
+++ packages/nextjs/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import NextAuth from "next-auth";
 import { authOptions, providers } from "~~/utils/auth";
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-async function auth(req: NextRequest, res: NextResponse) {
+async function auth(req: NextRequest, context: { params: Promise<{ nextauth: string[] }> }) {
   const { searchParams } = new URL(req.url as string);
   const isDefaultSigninPage = searchParams.get("nextauth")?.includes("signin");
 
@@ -12,7 +12,7 @@ async function auth(req: NextRequest, res: NextResponse) {
     providers.pop();
   }
 
-  return await NextAuth(req, res as unknown as { params: { nextauth: string[] } }, authOptions);
+  return await NextAuth(req, context, authOptions);
 }
 
 export { auth as GET, auth as POST };
